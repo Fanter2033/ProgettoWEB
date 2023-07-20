4d40823b86ecb1890edd9153151f25db9dc381c2
Fanter <119590859+Fanter2033@users.noreply.github.com>
2023-07-20 17:42:42 +0200

    prove autenticazione

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//testing per registrazione e login
+const bcrypt = require('bcrypt')
+
 //curl -X GET localhost:8000 && echo ""
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -27,11 +30,41 @@ app.delete('/test/', (req, res) => {
     res.send('<h1>Vamos a Campobasso</h1>');
 });
 
+
 app.get("/app/user/:username", (req, res) => {
     let username = req.params['username'];
     console.log("Username :", username);
     res.send('<h1>'+username+'</h1>');
 });
+
+//lista utenti temporanea
+const usersList = [];
+
+//creazione utente
+app.post("/app/user", (req, res) => {
+
+    try {
+        let PasswdSalt = bcrypt.genSalt()  //per capire il meccanismo del salt rifarsi alle slide di Renzo
+        let PasswdHased = bcrypt.hash(req.body.password, PasswdSalt);
+        const NewUser = { username: req.body.username, 
+                          password: PasswdHased };
+        console.log(NewUser); 
+        if(usersList.includes(NewUser.username)){ //username già esistente
+            usersList.push(NewUser.username);
+            res.status(201).send();
+        }
+        else
+            res.status(400).send();
+
+       
+    } catch {
+        res.status(422).send()
+    }
+
+
+
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
