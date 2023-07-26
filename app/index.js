efa72536235dd27d8d37e1b8ffe14979df0ea95f
Fanter <119590859+Fanter2033@users.noreply.github.com>
2023-07-26 18:42:49 +0200

    struttura file e tentativo login

--- app/index.js
+++ app/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+global.rootDir = __dirname ;
+global.startDate = null; 
+
+app.use('/js'  , express.static(global.rootDir +'/public/js'));
+app.use('/css' , express.static(global.rootDir +'/public/css'));
+app.use('/data', express.static(global.rootDir +'/public/data'));
+app.use('/docs', express.static(global.rootDir +'/public/html'));
+app.use('/img' , express.static(global.rootDir +'/public/media'));
 
 //testing per registrazione e login
 const bcrypt = require('bcrypt')
 
 //curl -X GET localhost:8000 && echo ""
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    //res.send('Hello World!');
+    res.sendFile(__dirname + '/public/html/index.html');
 });
 
 //curl -X GET localhost:8000/test/ && echo ""
@@ -15,10 +24,11 @@ app.get('/test/', (req, res) => {
     res.send('<h1>Vamos a bailar</h1>');
 });
 
+/*
 //curl -X POST localhost:8000/test/ && echo ""
 app.post('/test/', (req, res) => {
     res.send('<h1>Vamos a Faenza</h1>');
-});
+});*/
 
 //curl -X PUT localhost:8000/test/ && echo ""
 app.put('/test/', (req, res) => {
@@ -42,14 +52,15 @@ const usersList = [];
 
 //creazione utente
 app.post("/app/user", (req, res) => {
-
-    try {
+    console.log("creazione user");
+    res.send('password');
+    /*try {
         let PasswdSalt = bcrypt.genSalt()  //per capire il meccanismo del salt rifarsi alle slide di Renzo
         let PasswdHased = bcrypt.hash(req.body.password, PasswdSalt);
         const NewUser = { username: req.body.username, 
                           password: PasswdHased };
         console.log(NewUser); 
-        if(usersList.includes(NewUser.username)){ //username già esistente
+        if(!usersList.includes(NewUser.username)){ //username già esistente
             usersList.push(NewUser.username);
             res.status(201).send();
         }
@@ -59,11 +70,15 @@ app.post("/app/user", (req, res) => {
        
     } catch {
         res.status(422).send()
-    }
+    }*/
 
 
 
 });
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/html/home.html');
+  });
 
 
 app.listen(port, () => {
