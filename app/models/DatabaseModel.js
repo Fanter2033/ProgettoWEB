f41018203082517fb0f1266d562e1b9fe7249a2e
romanellas <www.daniele.romanella@gmail.com>
2023-08-08 22:16:44 +0200

    AV

--- app/models/DatabaseModel.js
+++ app/models/DatabaseModel.js
@@ -7,11 +7,7 @@ module.exports = class DatabaseModel {
         let conn = await autoload.mongoConnectionFunctions.getDatabaseConnection();
         await conn.connect().then();
         let database = await conn.db(autoload.config._DATABASE_NAME);
-        let collList = await database.listCollections().toArray();
-        collList.map((element) => {
-            return element.name;
-        });
-        return collList;
+        return await database.listCollections().toArray();
     }
 
 }
