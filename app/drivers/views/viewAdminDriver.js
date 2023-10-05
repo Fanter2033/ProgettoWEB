941d9e3d020fddadbeabdba07fa7bb97b33fd084
romanellas <www.daniele.romanella@gmail.com>
2023-10-05 12:58:45 +0200

    Avanzamento

--- app/drivers/views/viewAdminDriver.js
+++ app/drivers/views/viewAdminDriver.js
@@ -66,5 +66,101 @@ viewAdminDriver.get('/users', async function (req, res) {
     res.render(publicDir + "/html/admins/user.html", {username: req.session.user.username, icon_uri: iconUri});
 });
 
+viewAdminDriver.get('/squeal', async function (req, res) {
+    let authAdmin = await authController.isAuthAdmin(req);
+    if (authAdmin === false) {
+        res.redirect('./');
+        return;
+    }
+    //If we are here, we can send the page, cause them are an admin!
+    //IF NO IMAGE
+    let random = authController.getRandomInt(3);
+    let iconUri = '';
+    switch (random){
+        case 0:
+            iconUri = '../../media/icons/man.png';
+            break;
+        case 1:
+            iconUri = '../../media/icons/woman.png';
+            break;
+        default:
+            iconUri = '../../media/icons/not-binary.png';
+            break;
+    }
+    res.render(publicDir + "/html/admins/squeal.html", {username: req.session.user.username, icon_uri: iconUri});
+});
+
+viewAdminDriver.get('/ochannels', async function (req, res) {
+    let authAdmin = await authController.isAuthAdmin(req);
+    if (authAdmin === false) {
+        res.redirect('./');
+        return;
+    }
+    //If we are here, we can send the page, cause them are an admin!
+    //IF NO IMAGE
+    let random = authController.getRandomInt(3);
+    let iconUri = '';
+    switch (random){
+        case 0:
+            iconUri = '../../media/icons/man.png';
+            break;
+        case 1:
+            iconUri = '../../media/icons/woman.png';
+            break;
+        default:
+            iconUri = '../../media/icons/not-binary.png';
+            break;
+    }
+    res.render(publicDir + "/html/admins/ochannels.html", {username: req.session.user.username, icon_uri: iconUri});
+});
+
+viewAdminDriver.get('/uchannels', async function (req, res) {
+    let authAdmin = await authController.isAuthAdmin(req);
+    if (authAdmin === false) {
+        res.redirect('./');
+        return;
+    }
+    //If we are here, we can send the page, cause them are an admin!
+    //IF NO IMAGE
+    let random = authController.getRandomInt(3);
+    let iconUri = '';
+    switch (random){
+        case 0:
+            iconUri = '../../media/icons/man.png';
+            break;
+        case 1:
+            iconUri = '../../media/icons/woman.png';
+            break;
+        default:
+            iconUri = '../../media/icons/not-binary.png';
+            break;
+    }
+    res.render(publicDir + "/html/admins/uchannels.html", {username: req.session.user.username, icon_uri: iconUri});
+});
+
+viewAdminDriver.get('/tchannels', async function (req, res) {
+    let authAdmin = await authController.isAuthAdmin(req);
+    if (authAdmin === false) {
+        res.redirect('./');
+        return;
+    }
+    //If we are here, we can send the page, cause them are an admin!
+    //IF NO IMAGE
+    let random = authController.getRandomInt(3);
+    let iconUri = '';
+    switch (random){
+        case 0:
+            iconUri = '../../media/icons/man.png';
+            break;
+        case 1:
+            iconUri = '../../media/icons/woman.png';
+            break;
+        default:
+            iconUri = '../../media/icons/not-binary.png';
+            break;
+    }
+    res.render(publicDir + "/html/admins/tchannels.html", {username: req.session.user.username, icon_uri: iconUri});
+});
+
 module.exports = viewAdminDriver;
 
