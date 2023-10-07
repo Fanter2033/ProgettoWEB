a0e7f25cf0f429e2014c5e388fc6e9b5694d8e16
Daniele Romanella <www.daniele.romanella@gmail.com>
2023-10-07 17:57:53 +0200

    Fix errore di produzione

--- app/autoload/autoload.js
+++ app/autoload/autoload.js
@@ -11,12 +11,9 @@ exports.config = config;
 exports.mongoConnectionFunctions = mongoConnection;
 
 const logger = require('npmlog');
-const loggerFile = require('npmlog-file');
 
 exports.logger = logger;
-exports.loggerFile = loggerFile;
 exports.logRequests = function (requestObject, next) {
-    exports.loggerFile.write(autoload.logger, `logs/accesslog_${(new Date).toISOString().split('T')[0]}.txt`);
     exports.logger.info(`[${(new Date).toISOString().split('T')[0]} ${(new Date).toISOString().split('T')[1].split('.')[0]}][${requestObject.method}][${requestObject.socket.remoteAddress}] Has requested the document: ${requestObject.originalUrl}. \n Request body: ${JSON.stringify(requestObject.body)} \n`);
     next();
 }
