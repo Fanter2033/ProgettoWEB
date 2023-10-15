271ab7660ab3ecb1fdc012fad068612d35cc3582
Daniele Romanella <daniele.romanella@studio.unibo.it>
2023-10-15 21:51:43 +0200

    Avanzamento implementazione canali

--- app/drivers/channelDriver.js
+++ app/drivers/channelDriver.js
@@ -4,23 +4,23 @@ const channelDriver = express();
 channelDriver.use(express.json());
 channelDriver.use(express.urlencoded({extended: true}));
 channelDriver.post('/', async function (req, res) {
-    res.send("1");
+
 });
 
 channelDriver.get('/', async function (req, res) {
-    res.send("5");
+
 });
 
 channelDriver.put('/:channel', async function (req, res) {
-    res.send("2");
+
 });
 
 channelDriver.delete('/:channel', async function (req, res) {
-    res.send("3");
+
 });
 
 channelDriver.get('/:channel', async function (req, res) {
-    res.send("4");
+
 });
 
 
