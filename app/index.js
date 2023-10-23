5a1a55448768b25ddd5de4a16cf4edf2b18f1859
ElisaCasalini <elisa.casalini@studio.unibo.it>
2023-10-23 11:30:06 +0200

    test crone

--- app/index.js
+++ app/index.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const autoload = require('./autoload/autoload');
+const express = require("express");
+const autoload = require("./autoload/autoload");
 const backEndRouter = express();
-const databaseDriver = require('./drivers/databaseDriver');
-const authDriver = require('./drivers/authDriver.js')
-const userDriver = require('./drivers/userDriver');
-const channelDriver = require('./drivers/channelDriver');
-const viewDriver = require('./drivers/views/viewDriver');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
-const cronDaemon = require('node-cron');
+const databaseDriver = require("./drivers/databaseDriver");
+const authDriver = require("./drivers/authDriver.js");
+const userDriver = require("./drivers/userDriver");
+const channelDriver = require("./drivers/channelDriver");
+const viewDriver = require("./drivers/views/viewDriver");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cronDaemon = require("node-cron");
+
+const QuoteController = require("./controllers/QuoteController");
+const QuoteModel = require("./models/QuoteModel");
+let controller = new QuoteController(new QuoteModel());
 
 global.rootDir = __dirname;
 global.startDate = null;
@@ -19,53 +23,54 @@ global.autoload = autoload;
 const oneDay = 1000 * 60 * 60 * 24;
 
 /*
-* save and crypt sessions
-* */
-backEndRouter.use(session({
+ * save and crypt sessions
+ * */
+backEndRouter.use(
+  session({
     secret: autoload.config._SESSION_SECRET,
     store: MongoStore.create({
-        mongoUrl: `mongodb://${autoload.config._DATABASE_USER}:${autoload.config._DATABASE_PWD}@${autoload.config._DATABASE_HOST}:${autoload.config._DATABASE_PORT}/${autoload.config._DATABASE_NAME}${autoload.config._DATABASE_EXTRA}`,
-        collection: autoload.config._SESSION_COLLECTION
+      mongoUrl: `mongodb://${autoload.config._DATABASE_USER}:${autoload.config._DATABASE_PWD}@${autoload.config._DATABASE_HOST}:${autoload.config._DATABASE_PORT}/${autoload.config._DATABASE_NAME}${autoload.config._DATABASE_EXTRA}`,
+      collection: autoload.config._SESSION_COLLECTION,
     }),
     saveUninitialized: true,
-    cookie: {maxAge: oneDay},
-    resave: false
-}));
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
 
 // parsing the incoming data
 backEndRouter.use(express.json());
-backEndRouter.use(express.urlencoded({extended: true}));
+backEndRouter.use(express.urlencoded({ extended: true }));
 
 //Allow CORS
-backEndRouter.use(cors())
+backEndRouter.use(cors());
 
 // cookie parser middleware
 backEndRouter.use(cookieParser());
 
 /*log requests*/
-backEndRouter.get('*', (req, res, next) => {
-    autoload.logRequests(req, next);
+backEndRouter.get("*", (req, res, next) => {
+  autoload.logRequests(req, next);
 });
-backEndRouter.post('*', (req, res, next) => {
-    autoload.logRequests(req, next);
+backEndRouter.post("*", (req, res, next) => {
+  autoload.logRequests(req, next);
 });
-backEndRouter.put('*', (req, res, next) => {
-    autoload.logRequests(req, next);
+backEndRouter.put("*", (req, res, next) => {
+  autoload.logRequests(req, next);
 });
-backEndRouter.delete('*', (req, res, next) => {
-    autoload.logRequests(req, next);
-});
-
-backEndRouter.patch('*', (req, res, next) => {
-    autoload.logRequests(req, next);
+backEndRouter.delete("*", (req, res, next) => {
+  autoload.logRequests(req, next);
 });
 
+backEndRouter.patch("*", (req, res, next) => {
+  autoload.logRequests(req, next);
+});
 
-backEndRouter.use('/js', express.static(global.rootDir + '/public/js'));
-backEndRouter.use('/css', express.static(global.rootDir + '/public/css'));
-backEndRouter.use('/data', express.static(global.rootDir + '/public/data'));
-backEndRouter.use('/docs', express.static(global.rootDir + '/public/html'));
-backEndRouter.use('/img', express.static(global.rootDir + '/public/media'));
+backEndRouter.use("/js", express.static(global.rootDir + "/public/js"));
+backEndRouter.use("/css", express.static(global.rootDir + "/public/css"));
+backEndRouter.use("/data", express.static(global.rootDir + "/public/data"));
+backEndRouter.use("/docs", express.static(global.rootDir + "/public/html"));
+backEndRouter.use("/img", express.static(global.rootDir + "/public/media"));
 
 /*
 backEndRouter.get('/home', (req, res) => {
@@ -74,19 +79,54 @@ backEndRouter.get('/home', (req, res) => {
  */
 
 //If a request starts with /database is sent to database driver!
-backEndRouter.use('/auth', authDriver);
-backEndRouter.use('/database', databaseDriver);
-backEndRouter.use('/user', userDriver);
-backEndRouter.use('/channel', channelDriver);
-backEndRouter.use('/', viewDriver);
+backEndRouter.use("/auth", authDriver);
+backEndRouter.use("/database", databaseDriver);
+backEndRouter.use("/user", userDriver);
+backEndRouter.use("/channel", channelDriver);
+backEndRouter.use("/", viewDriver);
 
-//Cron handler
-cronDaemon.schedule("0 0 * * *", function () {
-    //Every day at 00:00 execute this code
-    console.log("Hey. It's midnight!")
+cronDaemon.schedule("0 0 * * *", async function () {
+  let start = await controller.dialyResetQuote();
+});
+
+cronDaemon.schedule("0 0 * * 1", async function () {
+  let start = await controller.weeklyResetQuote();
+});
+
+cronDaemon.schedule("0 0 1 * *", async function () {
+  let start = await controller.monthlyResetQuote();
 });
 
 
+
+
+//Cron handler: (sec) min  h  d_m  m  d_w
+
+//ogni 10 sec
+cronDaemon.schedule("*/10 * * * * *", function () {
+  console.log("10 sec!");
+});
+
+//ogni minuto
+cronDaemon.schedule("* * * * *", function () {
+  console.log("1 min!");
+});
+
+//Every day at 00:00 execute this code
+cronDaemon.schedule("0 0 * * *", function () {
+  console.log("Hey. It's midnight!");
+});
+
+//tutti i lunedì:
+cronDaemon.schedule("0 0 * * 1", function () {
+  console.log("Hey. It's Monday!");
+});
+
+//il primo di ogni mese:
+cronDaemon.schedule("0 0 1 * *", function () {
+  console.log("Hey. It's the first of the month!");
+});
+
 backEndRouter.listen(autoload.config._WEBSERVER_PORT, () => {
-    console.log(`Server started on port ${autoload.config._WEBSERVER_PORT}`);
+  console.log(`Server started on port ${autoload.config._WEBSERVER_PORT}`);
 });
