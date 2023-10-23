28521e1475a1767a67a4822d1c9042482242337f
Daniele Romanella <daniele.romanella@studio.unibo.it>
2023-10-23 13:56:39 +0200

    Avanzamento

--- app/drivers/views/viewDriver.js
+++ app/drivers/views/viewDriver.js
@@ -4,22 +4,25 @@ const path = require("path");
 const publicDir = path.join(__dirname, '../../public');
 viewDriver.use(express.static(publicDir));
 const viewAdminDriver = require('./viewAdminDriver');
-const reactDriver = require('./reactDriver');
 
 viewDriver.get('/', (req, res) => {
     res.sendFile(publicDir + "/html/deMultiplexPage.html");
 });
 
 //view di react
-viewDriver.use('/userView', reactDriver);
+viewDriver.get('/userView*', (req, res) => {
+    const path = require("path")
+    //serve the static files from react
+    const reactDir = path.join(__dirname, '../../react-squealer/build');
+    viewDriver.use(express.static(reactDir));
+    res.sendFile(path.join(reactDir, '/index.html'));
+});
 
 //view di smm
 viewDriver.get('/smm/', (req, res) => {
     res.send('HELLO! Cambiare qui (2)');
 });
 
-//view di admin
 viewDriver.use('/admin', viewAdminDriver);
 
 module.exports = viewDriver;
-
