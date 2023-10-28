6c8bab37551a6b3e182478acb27629767225c26c
Pietro Sami <pietro.sami@studio.unibo.it>
2023-10-28 19:39:22 +0200

    readme, swagger, schema e model degli Squeal

--- app/drivers/squealDriver.js
+++ app/drivers/squealDriver.js
@@ -3,13 +3,9 @@ const squealDriver = express();
 const SquealController = require("../controllers/SquealController");
 const SquealModel = require("../models/SquealModel");
 
-let controller = new SquealController(new );
-
-
+let controller = new SquealController(new SquealModel());
 
 squealDriver.use(express.json());
-squealDriver.use(express.urlencoded({extended: true}))
+squealDriver.use(express.urlencoded({extended: true}));
 
-squealDriver.get('/:identifier', async function (req,
 
-)
