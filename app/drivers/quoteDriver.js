d11025135cd9b596ca457d61263cf5feecb8e63b
Daniele Romanella <daniele.romanella@studio.unibo.it>
2023-11-23 16:15:57 +0100

    Avanzamento

--- app/drivers/quoteDriver.js
+++ app/drivers/quoteDriver.js
@@ -24,4 +24,13 @@ quoteDriver.patch("/", async function (req, res) {
   else res.status(ctrlOut.code).send(ctrlOut);
 });
 
+quoteDriver.patch("/buy/", async function (req, res) {
+  let userAuth = await authController.getAuthenticatedUser(req);
+  let username = typeof req.params["username"] !== "undefined" ? req.params["username"] : "";
+  let percentage = typeof req.body["percentage"] !== "undefined" ? req.body["percentage"] : -1;
+  let ctrlOut = await controller.applyPercentageQuote(userAuth, username, percentage, true);
+  if (ctrlOut.code === 200) res.status(ctrlOut.code).send(ctrlOut.content);
+  else res.status(ctrlOut.code).send(ctrlOut);
+});
+
 module.exports = quoteDriver;
