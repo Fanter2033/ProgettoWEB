26e87c80ea303427b86d910f442d54c1656d8bb0
romanellas <www.daniele.romanella@gmail.com>
2024-02-07 10:44:03 +0100

    Deleted cretentials

--- app/config/squealer.js
+++ app/config/squealer.js
@@ -10,19 +10,19 @@ exports._WEBSERVER_PORT = 8000;
 
 //Mongo db database
 if(exports.isDebug){
-    exports._DATABASE_USER = "site222317";
-    exports._DATABASE_PWD = "tagira5A";
-    exports._DATABASE_HOST = "217.61.59.180";    //Aka alpaca-vps
-    exports._DATABASE_PORT = '7917';
-    exports._DATABASE_NAME = "site222317";
+    exports._DATABASE_USER = "";
+    exports._DATABASE_PWD = "";
+    exports._DATABASE_HOST = "";
+    exports._DATABASE_PORT = '';
+    exports._DATABASE_NAME = "";
     exports._DATABASE_EXTRA = "";
 }else{
-    exports._DATABASE_USER = "site222317";
-    exports._DATABASE_PWD = "tagira5A";
-    exports._DATABASE_HOST = "mongo_site222317"; //macchina lab dimmerda
-    exports._DATABASE_PORT = '27017';
-    exports._DATABASE_NAME = "site222317";
-    exports._DATABASE_EXTRA = "?authSource=admin&writeConcern=majority";
+    exports._DATABASE_USER = "";
+    exports._DATABASE_PWD = "";
+    exports._DATABASE_HOST = "";
+    exports._DATABASE_PORT = '';
+    exports._DATABASE_NAME = "";
+    exports._DATABASE_EXTRA = "";
 }
 
 //Regex
@@ -68,6 +68,6 @@ exports._UNPOPULAR_QUOTE_POSTS = 3;
 exports._LIMIT_RETURN_POSTS = 250;
 
 //Session params
-exports._SESSION_SECRET = '5DE1ya358SlHeJY52MbH8SHssFJqQh8TZVAyiVKBGDuCl1ZmFlXMqRTRj8hzSqFUJ5iPZwzKt1ERhqR0oabSMFSVxV9HM7KNZhkhQ1wdpkgY70QB5wixx5QvFbeV0uml';
+exports._SESSION_SECRET = '';
 
 
